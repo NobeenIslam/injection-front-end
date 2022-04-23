8dfa94f750e31ded23783c44bda621709f111104
Nobeen <n.islam.1@outlook.com>
2022-04-23 17:08:38 +0100

    Imported axios and prod/dev NODEENV switch for express

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { greet } from "./utils/greet";
+import axios from 'axios'
 
 function App(): JSX.Element {
+
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://todolistnobeen.herokuapp.com"
+      : "http://localhost:4000";
+
+
   return <h1>{greet("World")}</h1>;
 }
 
